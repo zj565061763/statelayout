3b506ad79c13e23bd45d7a913c6775bc7f235f78
zjun <565061763@qq.com>
2021-07-16 23:09:30 +0800

    SourceEmptyStrategy

--- lib/src/main/java/com/sd/lib/statelayout/empty/SourceEmptyStrategy.java
+++ lib/src/main/java/com/sd/lib/statelayout/empty/SourceEmptyStrategy.java
@@ -1,14 +1,18 @@
 package com.sd.lib.statelayout.empty;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.lang.ref.WeakReference;
 
 public abstract class SourceEmptyStrategy<S> implements FStateEmptyStrategy {
     private final WeakReference<S> mSource;
 
-    public SourceEmptyStrategy(S source) {
-        mSource = source == null ? null : new WeakReference<S>(source);
+    public SourceEmptyStrategy(@Nullable S source) {
+        mSource = source == null ? null : new WeakReference<>(source);
     }
 
+    @Nullable
     public final S getSource() {
         return mSource == null ? null : mSource.get();
     }
@@ -18,11 +22,13 @@ public abstract class SourceEmptyStrategy<S> implements FStateEmptyStrategy {
         return getSource() == null;
     }
 
+    @NonNull
     @Override
     public final Result getResult() {
         final S source = getSource();
-        if (source == null)
+        if (source == null) {
             return Result.None;
+        }
 
         return getResultImpl(source);
     }
