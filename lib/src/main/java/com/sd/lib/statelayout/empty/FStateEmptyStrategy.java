efb5a7759292844e1cf98f5e49446215199d2ea4
zjun <565061763@qq.com>
2021-07-16 14:31:30 +0800

    FStateEmptyStrategy

--- lib/src/main/java/com/sd/lib/statelayout/empty/FStateEmptyStrategy.java
+++ lib/src/main/java/com/sd/lib/statelayout/empty/FStateEmptyStrategy.java
@@ -1,11 +1,14 @@
 package com.sd.lib.statelayout.empty;
 
+import androidx.annotation.NonNull;
+
 /**
- * 显示无内容状态策略接口
+ * 无内容状态策略接口
  */
 public interface FStateEmptyStrategy {
     boolean isDestroyed();
 
+    @NonNull
     Result getResult();
 
     enum Result {
