6ed8edd73b76338c692dfedecfec1f47a7d30249
zjun <565061763@qq.com>
2021-07-16 10:52:01 +0800

    annotation

--- lib/src/main/java/com/sd/lib/statelayout/IStateView.java
+++ lib/src/main/java/com/sd/lib/statelayout/IStateView.java
@@ -2,18 +2,21 @@ package com.sd.lib.statelayout;
 
 import android.view.View;
 
+import androidx.annotation.IdRes;
+import androidx.annotation.Nullable;
+
 public interface IStateView {
     /**
      * 设置显示内容
      *
      * @param layoutId 内容view布局id
      */
-    void setContentView(int layoutId);
+    void setContentView( int layoutId);
 
     /**
      * 设置显示内容
      *
      * @param contentView 内容view
      */
-    void setContentView(View contentView);
+    void setContentView(@Nullable View contentView);
 }
--- lib/src/main/java/com/sd/lib/statelayout/SimpleStateView.java
+++ lib/src/main/java/com/sd/lib/statelayout/SimpleStateView.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
 
+import androidx.annotation.Nullable;
+
 class SimpleStateView extends FrameLayout implements IStateView {
     public SimpleStateView(Context context) {
         super(context);
@@ -12,14 +14,17 @@ class SimpleStateView extends FrameLayout implements IStateView {
 
     @Override
     public void setContentView(int layoutId) {
-        final View view = layoutId == 0 ? null : LayoutInflater.from(getContext()).inflate(layoutId, this, false);
+        final View view = layoutId == 0 ?
+                null :
+                LayoutInflater.from(getContext()).inflate(layoutId, this, false);
         setContentView(view);
     }
 
     @Override
-    public void setContentView(View contentView) {
+    public void setContentView(@Nullable View contentView) {
         removeAllViews();
-        if (contentView != null)
+        if (contentView != null) {
             addView(contentView);
+        }
     }
 }
